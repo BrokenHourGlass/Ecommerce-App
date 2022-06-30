907e1ad502a3575c96d625b8644bc81dfbc98e5c
Vincent Salinas <vincent.salinas.2022@gmail.com>
2022-06-30 12:49:05 -0700

    Fixed issue with collection view not showing all rows.

--- ecommerce/ecommerce/ShoppingCartPage/Views/ShoppingCartSVC.swift
+++ ecommerce/ecommerce/ShoppingCartPage/Views/ShoppingCartSVC.swift
@@ -10,6 +10,8 @@ import SwiftUI
 struct ShoppingCartSVC: View {
     var modelData = ModelData()
     
+    private var gridItemLayout = [GridItem(.flexible())]
+    
     var body: some View {
         VStack {
             NavigationBar()
@@ -22,23 +24,26 @@ struct ShoppingCartSVC: View {
             }
             .padding([.leading, .trailing], 27)
             .padding([.top, .bottom], 15)
-            List {
-                ForEach(modelData.products) { product in
-                    HStack(spacing: 15) {
-                        product.img
-                            .resizable()
-                            .frame(width: 100, height: 100)
-                        VStack(alignment: .leading, spacing: 5) {
-                            Text(product.name)
-                                .fontWeight(.bold)
-                                .foregroundColor(.black)
-                                .lineLimit(1)
-                            Text("$ \(product.price)")
+            ScrollView(.vertical) {
+                LazyVGrid(columns: gridItemLayout) {
+                    ForEach(0..<modelData.products.count) { index in
+                        HStack(spacing: 15) {
+                            modelData.products[index].img
+                                .resizable()
+                                .frame(width: 100, height: 100)
+                            VStack(alignment: .leading, spacing: 5) {
+                                Text(modelData.products[index].name)
+                                    .fontWeight(.bold)
+                                    .foregroundColor(.black)
+                                    .lineLimit(1)
+                                Text("$ \(modelData.products[index].price)")
+                            }
+                            AddRemoveItem()
                         }
-                        AddRemoveItem()
                     }
                 }
             }
+            .padding([.leading, .trailing], 24)
             VStack(spacing: 10) {
                 HStack {
                     Spacer()
