baa63d875dbcf97b0354a86b6b87d3df48b14979
Vincent Salinas <vincent.salinas.2022@gmail.com>
2022-06-29 17:45:14 -0700

    Implemented SwiftUI view for Shopping Cart page.

---
+++ ecommerce/ecommerce/ShoppingCartPage/Views/ShoppingCartSVC.swift
@@ -0,0 +1,20 @@
+//
+//  ShoppingCartSVC.swift
+//  ecommerce
+//
+//  Created by Vincent Salinas on 6/29/22.
+//
+
+import SwiftUI
+
+struct ShoppingCartSVC: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ShoppingCartSVC_Previews: PreviewProvider {
+    static var previews: some View {
+        ShoppingCartSVC()
+    }
+}
