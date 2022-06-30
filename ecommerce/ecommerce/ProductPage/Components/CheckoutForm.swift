43a34a3a9e429e89c222df7445a2338b90b0fd1e
Vincent Salinas <vincent.salinas.2022@gmail.com>
2022-06-30 13:47:40 -0700

    Added ability to scroll through Checkout form.

--- ecommerce/ecommerce/ProductPage/Components/CheckoutForm.swift
+++ ecommerce/ecommerce/ProductPage/Components/CheckoutForm.swift
@@ -8,18 +8,38 @@
 import SwiftUI
 
 struct CheckoutForm: View {
+    let columns: [GridItem] = [
+        GridItem(.flexible())
+    ]
+    
+    
     var body: some View {
         VStack(alignment: .leading, spacing: 15) {
             Text("CHECKOUT")
                 .font(.title)
-            CheckoutField(title: "Name", placeholder: "Enter name here")
-            CheckoutField(title: "Email", placeholder: "Enter name email")
-            CheckoutField(title: "Phone Number", placeholder: "+1202-555-0136")
             
-            CheckoutField(title: "Your Address", placeholder: "1137 Williams Avenue")
-            CheckoutField(title: "ZIP Code", placeholder: "10001")
-            CheckoutField(title: "City", placeholder: "New York")
-            CheckoutField(title: "Country", placeholder: "United States")
+            ScrollView {
+                LazyVGrid(columns: columns, alignment: .leading) {
+                    Text("BILLING DETAILS")
+                        .foregroundColor(.red)
+                        .padding([.top], 15)
+                        .padding([.bottom], 10)
+                    CheckoutField(title: "Name", placeholder: "Enter name here")
+                    CheckoutField(title: "Email", placeholder: "Enter name email")
+                    CheckoutField(title: "Phone Number", placeholder: "+1202-555-0136")
+                    
+                    Text("BILLING DETAILS")
+                        .foregroundColor(.red)
+                        .padding([.top], 30)
+                        .padding([.bottom], 10)
+                    CheckoutField(title: "Your Address", placeholder: "1137 Williams Avenue")
+                    CheckoutField(title: "ZIP Code", placeholder: "10001")
+                    CheckoutField(title: "City", placeholder: "New York")
+                    CheckoutField(title: "Country", placeholder: "United States")
+                }
+                
+            }
+            
         }
         .padding([.leading, .trailing], 27)
         .padding([.top], 20)
