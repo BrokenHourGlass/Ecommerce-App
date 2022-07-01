686fc760862ad3c9e2d5d7c50808df223e270168
Vincent Salinas <vincent.salinas.2022@gmail.com>
2022-06-30 17:54:31 -0700

    Added all image assets.

---
+++ ecommerce/ecommerce/ProductPage/Views/ProductVC.swift
@@ -0,0 +1,29 @@
+//
+//  ProductVC.swift
+//  ecommerce
+//
+//  Created by Vincent Salinas on 6/30/22.
+//
+
+import UIKit
+
+class ProductVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
