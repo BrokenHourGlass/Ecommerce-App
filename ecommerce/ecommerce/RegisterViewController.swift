508499f68d5b9af36b9887b204ab99d1ca4e5ad9
AARON CARLSON <aaron.carlson88@gmail.com>
2022-07-01 15:06:28 -0700

    yo

---
+++ ecommerce/ecommerce/RegisterViewController.swift
@@ -0,0 +1,35 @@
+//
+//  RegisterViewController.swift
+//  ecommerce
+//
+//  Created by xcode on 6/29/22.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+     @IBAction func Register(_ sender: Any) {
+     }
+     // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    @IBAction func otp(_ sender: Any) {
+    }
+    @IBAction func registerButton(_ sender: Any) {
+    }
+    
+}
