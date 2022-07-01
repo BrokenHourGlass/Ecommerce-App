6c9b385ed01b359564388b3faf802670c780dcaf
Vincent Salinas <vincent.salinas.2022@gmail.com>
2022-07-01 09:11:43 -0700

    Fixed typo in About component.

--- ecommerce/ecommerce/Shared/About/About.swift
+++ ecommerce/ecommerce/Shared/About/About.swift
@@ -21,7 +21,7 @@ struct About: View {
                 .font(.title)
                 .bold()
                 .multilineTextAlignment(.center)
-            Text("Located at the heart of New York City, Audiophile is the premier store for high end headphones, earphones, speakers, and audio accessories. We have a large showroom and luxury demonstration rooms available for you to browse and experience a wide range of our products. Stop by our store to meet some of the fantastic people who make Audiophile the best place to buy your portable audio equipment.")
+            Text("Located at the heart of New York City, Amazona is the premier store for high end headphones, earphones, speakers, and audio accessories. We have a large showroom and luxury demonstration rooms available for you to browse and experience a wide range of our products. Stop by our store to meet some of the fantastic people who make Amazona the best place to buy your portable audio equipment.")
                 .multilineTextAlignment(.center)
         }
         .padding([.leading, .trailing], 27)
