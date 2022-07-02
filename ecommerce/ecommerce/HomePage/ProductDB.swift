86dd98431630d7ed8e5a1632b3ef7a89fd4eee5c
Vincent Salinas <vincent.salinas.2022@gmail.com>
2022-07-01 19:35:33 -0700

    Added Home page.

---
+++ ecommerce/ecommerce/HomePage/ProductDB.swift
@@ -0,0 +1,15 @@
+//
+//  ProductDB.swift
+//  ecommerce
+//
+//  Created by admin on 7/1/22.
+//
+
+import Foundation
+import SQLite3
+
+//class ProductDB{
+//    static var dib = ProductDB() //static instance
+//    var point: OpaquePointer?
+//    var items = [Item]() //instance storage
+//}
