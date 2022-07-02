3ef0e84c9e8cc5b097f558240f49771012db36b9
Jacob <jacobdigby@protonmail.com>
2022-07-01 20:30:13 -0400

    setup easy adjustment of design, separated design elements and text into their own files

--- ecommerce/ecommerce/HomePage.swift
+++ ecommerce/ecommerce/HomePage.swift
@@ -9,31 +9,27 @@ import UIKit
 import SwiftUI
 
 struct HomePage: View {
-    @State private var query: String = ""
-    private var design = elements()
     private var details = Info()
+    let design = elements()
+    var setup: HomeLayout
+    
+    @State var query = ""
+    
+    init(){
+        self.setup = HomeLayout(primary: design.primaryColor, secondary: design.secondaryColor, Accent: design.accent)
+    }
     
     func catalogSnippet(img: String, name: String, link: String) -> some View{
         return ZStack{
-            Image(img).resizable().background(Capsule().fill(design.primaryColor).frame(width: 250, height: 250, alignment: .center)).frame(width: 350, height: 250, alignment: .center)
-            Text(name).foregroundColor(design.accent)
+            Image(img).resizable().background(Capsule().fill(setup.primary).frame(width: 250, height: 250, alignment: .center)).frame(width: 350, height: 250, alignment: .center)
+            Text(name).foregroundColor(setup.Accent)
         }
     }
     
         var body: some View {
-            VStack{
-                HStack{
-                    Text("/ShopIT").foregroundColor(design.secondaryColor).font(.system(size: 28))
-                    Spacer()
-                    Button(action:{
-                        print("clicked")
-                    })
-                    {
-                        Image(systemName: "cart").renderingMode(Image.TemplateRenderingMode?.init(Image.TemplateRenderingMode.original)).resizable().frame(width: 30, height: 28, alignment: .leading)
-                    }
-                }.frame(width: .infinity, height: 50, alignment: .center).background(design.primaryColor)
+            setup.topBar()
             HStack{
-                TextField("Search", text: $query).foregroundColor(design.primaryColor)
+                TextField("Search", text: $query).foregroundColor(setup.primary)
             }
             ScrollView {
                 ZStack{
@@ -41,10 +37,10 @@ struct HomePage: View {
                     Image("adBackground")
                     VStack{
                         Spacer()
-                        Text("New in Stock").foregroundColor(design.secondaryColor).font(.system(size: 28))
+                        Text("New in Stock").foregroundColor(setup.secondary).font(.system(size: 28))
                         Spacer()
-                        Text("Corsair").foregroundColor(design.secondaryColor).font(.system(size: 24))
-                        Text("Desktop PC").foregroundColor(design.secondaryColor).font(.system(size: 24))
+                        Text("Corsair").foregroundColor(setup.secondary).font(.system(size: 24))
+                        Text("Desktop PC").foregroundColor(setup.secondary).font(.system(size: 24))
                         Spacer()
                         Spacer()
                     }
@@ -58,27 +54,18 @@ struct HomePage: View {
                     catalogSnippet(img: "home", name: "bob", link: "bob")
                 }
                 VStack{
-                    Text("signup here").foregroundColor(design.secondaryColor)
-                }.frame(width: 5000, height: 500, alignment: .center).background(design.primaryColor)
+                    Text("signup here").foregroundColor(setup.secondary)
+                }.frame(width: 5000, height: 500, alignment: .center).background(setup.primary)
+                setup.companySummary()
                 HStack{
                     VStack{
-                        HStack{
-                            Text("BEST").foregroundColor(design.accent)
-                            Text(details.summaryTitle).foregroundColor(design.primaryColor)
-                        }
-                        Image("inspire")
-                        Text(details.summmary).multilineTextAlignment(.center).foregroundColor(design.primaryColor).frame(maxWidth: 300, alignment: .center)
-                    }.frame(maxWidth: .infinity).background{design.secondaryColor}
-                }
-                HStack{
-                    VStack{
-                        Text(details.legal).multilineTextAlignment(.center).foregroundColor(design.secondaryColor).frame(maxWidth: 300, alignment: .center)
-                    }.frame(maxWidth: .infinity).background(design.primaryColor)
+                        Text(details.legal).multilineTextAlignment(.center).foregroundColor(setup.secondary).frame(maxWidth: 300, alignment: .center)
+                    }.frame(maxWidth: .infinity).background(setup.primary)
                 }
             }
         }
     }
-}
+
 
 class MainPage: UIHostingController<HomePage>{
     required init?(coder: NSCoder) {
