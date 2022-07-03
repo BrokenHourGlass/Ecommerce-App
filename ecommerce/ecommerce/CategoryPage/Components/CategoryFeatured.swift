c8153b5fd90a24d215abd087f827b8a0611e8267
Vincent Salinas <vincent.salinas.2022@gmail.com>
2022-07-03 12:48:53 -0700

    Finished creating featured section.

--- ecommerce/ecommerce/CategoryPage/Components/CategoryFeatured.swift
+++ ecommerce/ecommerce/CategoryPage/Components/CategoryFeatured.swift
@@ -10,23 +10,31 @@ import SwiftUI
 struct CategoryFeatured: View {
     var modelData = products
     
-    let rows: [GridItem] = [GridItem(.fixed(100))]
+    let rows: [GridItem] = [GridItem(.flexible())]
     
     
     var body: some View {
         ScrollView(.horizontal) {
             LazyHGrid(rows: rows) {
                 ForEach(modelData, id: \.id) { product in
-                    Image(product.productIMG)
-                        .resizable()
-                        .frame(maxWidth: 100, maxHeight: 100)
-                        .scaledToFill()
-                        .clipped()
-                        .listRowInsets(EdgeInsets())
+                    HStack {
+                        VStack {
+                            Image(product.productIMG)
+                                .resizable()
+                                .frame(maxWidth: 100, maxHeight: 100)
+                                .scaledToFill()
+                                .clipped()
+                                .listRowInsets(EdgeInsets())
+                                .cornerRadius(10)
+                            Text(product.name)
+                                .bold()
+                                .font(.caption2)
+                            
+                        }
+                        .frame(width: 100)
+                    }
                 }
-                
             }
-            
         }
     }
 }
