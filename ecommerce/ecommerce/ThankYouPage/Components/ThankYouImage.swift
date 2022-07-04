f7c38d2361e59fd47b887271cb45674b7525cb54
Vincent Salinas <vincent.salinas.2022@gmail.com>
2022-07-03 21:16:32 -0700

    Created ThankYouImage component.

--- ecommerce/ecommerce/ThankYouPage/Components/ThankYouImage.swift
+++ ecommerce/ecommerce/ThankYouPage/Components/ThankYouImage.swift
@@ -9,7 +9,16 @@ import SwiftUI
 
 struct ThankYouImage: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            Image(systemName: "checkmark.circle.fill")
+                .resizable()
+                .frame(width: 100, height: 100)
+                .scaledToFill()
+                .clipped()
+                .listRowInsets(EdgeInsets())
+                .foregroundColor(Color.red)
+            Spacer()
+        }
     }
 }
 
