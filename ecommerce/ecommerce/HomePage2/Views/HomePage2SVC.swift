caca22f436f7ddc4ce6e4c20b5fc14b39d7520f5
Vincent Salinas <vincent.salinas.2022@gmail.com>
2022-07-05 08:51:42 -0700

    Implemented SwiftUI view for Home page.

---
+++ ecommerce/ecommerce/HomePage2/Views/HomePage2SVC.swift
@@ -0,0 +1,20 @@
+//
+//  HomePage2SVC.swift
+//  ecommerce
+//
+//  Created by Vincent Salinas on 7/5/22.
+//
+
+import SwiftUI
+
+struct HomePage2SVC: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomePage2SVC_Previews: PreviewProvider {
+    static var previews: some View {
+        HomePage2SVC()
+    }
+}
