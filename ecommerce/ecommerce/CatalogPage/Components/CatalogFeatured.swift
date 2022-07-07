1ae1a2e03ae08022bd455409290d72f66c7d372b
Vincent Salinas <vincent.salinas.2022@gmail.com>
2022-07-06 19:52:18 -0700

    Created CatalogFeatured component.

--- ecommerce/ecommerce/CatalogPage/Components/CatalogFeatured.swift
+++ ecommerce/ecommerce/CatalogPage/Components/CatalogFeatured.swift
@@ -8,13 +8,34 @@
 import SwiftUI
 
 struct CatalogFeatured: View {
+    @EnvironmentObject var cartManager: CartManager
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading) {
+            Text("Featured Now!")
+                .font(.title)
+                .bold()
+            Text("Check out all the new highly recommended swag")
+                .font(.title3)
+            ScrollView(.horizontal) {
+                LazyHStack {
+                    ForEach(0..<products.count) { index in
+                        NavigationLink(destination: ProductSVC(product: products[index]).environmentObject(cartManager)) {
+                            HomePageProduct(product: products[index])
+                        }
+                    }
+                }
+            }
+        }
+        .padding([.leading, .top, .bottom], 15)
+        .foregroundColor(Color.white)
+        .background(Color.pink)
     }
 }
 
 struct CatalogFeatured_Previews: PreviewProvider {
     static var previews: some View {
         CatalogFeatured()
+            .environmentObject(CartManager())
     }
 }
