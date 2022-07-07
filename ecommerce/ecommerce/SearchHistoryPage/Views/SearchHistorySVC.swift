c132922e5316c28a74245c903ee4c5456fecf4c0
Vincent Salinas <vincent.salinas.2022@gmail.com>
2022-07-07 08:17:07 -0700

    Setup SearchHistory swiftui view through view controller.

---
+++ ecommerce/ecommerce/SearchHistoryPage/Views/SearchHistorySVC.swift
@@ -0,0 +1,20 @@
+//
+//  SearchHistorySVC.swift
+//  ecommerce
+//
+//  Created by Vincent Salinas on 7/7/22.
+//
+
+import SwiftUI
+
+struct SearchHistorySVC: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SearchHistorySVC_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchHistorySVC()
+    }
+}
