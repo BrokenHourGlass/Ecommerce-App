73b5c3e309ae2efb834ee127bd75a515e4890928
Vincent Salinas <vincent.salinas.2022@gmail.com>
2022-07-07 07:06:34 -0700

    Fixed issue with navigation link with Category page.

---
+++ ecommerce/ecommerce/CategoryPage/Components/CategoryFeaturedProduct.swift
@@ -0,0 +1,20 @@
+//
+//  CategoryFeaturedProduct.swift
+//  ecommerce
+//
+//  Created by Vincent Salinas on 7/7/22.
+//
+
+import SwiftUI
+
+struct CategoryFeaturedProduct: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CategoryFeaturedProduct_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryFeaturedProduct()
+    }
+}
