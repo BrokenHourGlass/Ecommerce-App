d9547e0b7368437c937f462e7834702837df9a72
Vincent Salinas <vincent.salinas.2022@gmail.com>
2022-07-08 08:24:47 -0700

    Implemented basic OrdersHistory model.

---
+++ ecommerce/ecommerce/Models/OrdersHistory.swift
@@ -0,0 +1,30 @@
+//
+//  OrdersHistory.swift
+//  ecommerce
+//
+//  Created by Vincent Salinas on 7/8/22.
+//
+
+import Foundation
+
+class OrdersHistory: ObservableObject {
+    @Published private(set) var orders: [Order] = []
+    
+    func addToHistory(cart: [CartItem]) {
+        orders.append(Order(items: cart))
+    }
+    
+    func getHistory() -> [Order] {
+        return orders
+    }
+}
+
+class Order {
+    var id = UUID()
+    var date = Date()
+    var items: [CartItem]
+    
+    init(items: [CartItem]) {
+        self.items = items
+    }
+}
