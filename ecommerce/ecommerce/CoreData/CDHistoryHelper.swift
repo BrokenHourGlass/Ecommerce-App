ad1fce48ebc7ca7b9b423c34a2e73f0c1cad0682
Vincent Salinas <vincent.salinas.2022@gmail.com>
2022-07-07 17:01:26 -0700

    Implemented addItem core data function for SearchHistory page.

--- ecommerce/ecommerce/CoreData/CDHistoryHelper.swift
+++ ecommerce/ecommerce/CoreData/CDHistoryHelper.swift
@@ -15,20 +15,37 @@ class CDHistoryHelper {
     
     let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext
     
-    func addProduct(product: NewProduct) {
+    func addProduct(productObj: NewProduct) {
         
         let productEntity = NSEntityDescription.insertNewObject(forEntityName: "Product", into: context!) as! Product
         
-        productEntity.id = Int64(product.id)
-        productEntity.name = product.name
-        productEntity.category = product.category
-        productEntity.new = product.new
-        productEntity.price = Int64(product.price)
-        productEntity.featured = product.featured
-        productEntity.cartIMG = product.cartIMG
-        productEntity.productIMG = product.productIMG
-        productEntity.pDescription = product.description
-        productEntity.features = product.features
+        productEntity.id = Int64(productObj.id)
+        productEntity.name = productObj.name
+        productEntity.category = productObj.category
+        productEntity.new = productObj.new
+        productEntity.price = Int64(productObj.price)
+        productEntity.featured = productObj.featured
+        productEntity.cartIMG = productObj.cartIMG
+        productEntity.productIMG = productObj.productIMG
+        productEntity.pDescription = productObj.description
+        productEntity.features = productObj.features
+        
+        do {
+            try context?.save()
+            print("data saved successfully")
+        } catch {
+            print("data save unsuccessful")
+            print(error.localizedDescription)
+        }
+    }
+    
+    func addItem(pid: Int, itemObj: NewItem) {
+        
+        let itemEntity = NSEntityDescription.insertNewObject(forEntityName: "Item", into: context!) as! Item
+        
+        itemEntity.pid = Int64(pid)
+        itemEntity.item = itemObj.item
+        itemEntity.quantity = Int64(itemObj.quantity)
         
         do {
             try context?.save()
