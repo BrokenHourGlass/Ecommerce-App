50334556572210c23369bb6bef0aa6ce32c7a587
Vincent Salinas <vincent.salinas.2022@gmail.com>
2022-07-08 17:20:34 -0700

    Implemented addOrder and getOrders core data functionalities.

---
+++ ecommerce/ecommerce/Entities/PNPOrder+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  PNPOrder+CoreDataClass.swift
+//  ecommerce
+//
+//  Created by Vincent Salinas on 7/8/22.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(PNPOrder)
+public class PNPOrder: NSManagedObject {
+
+}
