c41d67f4dd2a19b39bd00fe6bf3be5718a7ea8ab
Vincent Salinas <vincent.salinas.2022@gmail.com>
2022-07-09 22:53:54 -0700

    Moved NavigationLink outside of ForEach loop for CatalogFeatured component.

--- ecommerce/ecommerce/CatalogPage/Components/CatalogFeatured.swift
+++ ecommerce/ecommerce/CatalogPage/Components/CatalogFeatured.swift
@@ -12,6 +12,9 @@ struct CatalogFeatured: View {
     @EnvironmentObject var historyManager: HistoryManager
     @EnvironmentObject var ordersManager: OrdersManager
     
+    @State var showNextView = false
+    @State var current = 0
+    
     var body: some View {
         VStack(alignment: .leading) {
             Text("Featured Now!")
@@ -19,15 +22,22 @@ struct CatalogFeatured: View {
                 .bold()
             Text("Check out all the new highly recommended swag")
                 .font(.title3)
+            NavigationLink(destination: ProductSVC(product: products[current]).environmentObject(cartManager).environmentObject(historyManager).environmentObject(ordersManager), isActive: $showNextView) {
+                EmptyView()
+            }
             ScrollView(.horizontal) {
                 LazyHStack {
                     ForEach(0..<products.count) { index in
-                        NavigationLink(destination: ProductSVC(product: products[index]).environmentObject(cartManager).environmentObject(historyManager).environmentObject(ordersManager)) {
+                        Button(action: {
+                            current = index
+                            showNextView = true
+                        }) {
                             HomePageProduct(product: products[index])
                         }
                     }
                 }
             }
+            .padding([.top], 10)
         }
         .padding([.leading, .top, .bottom], 15)
         .foregroundColor(Color.white)
