511c71da2eb9a70ec843746995272ea014cea5ff
Vincent Salinas <vincent.salinas.2022@gmail.com>
2022-07-11 19:09:46 -0700

    Added addNewNote button.

--- ecommerce/ecommerce/CommentPage/Views/CommentPageSVC.swift
+++ ecommerce/ecommerce/CommentPage/Views/CommentPageSVC.swift
@@ -13,8 +13,21 @@ struct CommentPageSVC: View {
     
     var body: some View {
         VStack(alignment: .leading) {
-            Text("Comments")
-                .font(.title)
+            HStack {
+                Text("Comments")
+                    .font(.title)
+                    .bold()
+                Spacer()
+                Button {
+                    print("Add new comment")
+                } label: {
+                    Image(systemName: "plus")
+                }
+                .padding([.top, .bottom, .leading, .trailing], 10)
+                .foregroundColor(Color.white)
+                .background(Color.accentColor)
+                .cornerRadius(10)
+            }
             ScrollView {
                 ForEach(notes, id: \.username) { note in
                     CommentItem(note: note)
