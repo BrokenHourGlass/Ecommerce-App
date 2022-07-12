f2f96400681a9bbf6ea9cb1111f83a2200d49859
Vincent Salinas <vincent.salinas.2022@gmail.com>
2022-07-11 19:40:07 -0700

    Created basic layout for Comment page for adding new comments.

--- ecommerce/ecommerce/CommentPage/Views/CommentSVC.swift
+++ ecommerce/ecommerce/CommentPage/Views/CommentSVC.swift
@@ -8,8 +8,30 @@
 import SwiftUI
 
 struct CommentSVC: View {
+    
+    @State var titleText: String = ""
+    @State var bodyText: String = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading) {
+            Text("Title")
+            TextField("Enter title here", text: $titleText)
+            Text("Body")
+            TextField("Enter body text here", text: $bodyText)
+            Spacer()
+            Button {
+                print("Added new comment")
+            } label: {
+                Text("Submit")
+                    .font(.title2)
+                    .frame(maxWidth: .infinity)
+                    .border(Color.red)
+            }
+            .padding()
+            .foregroundColor(Color.white)
+            .background(Color.red)
+        }
+        .padding()
     }
 }
 
