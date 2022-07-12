d5268418d9719a40e95f90058c9ca51c4233b0db
Vincent Salinas <vincent.salinas.2022@gmail.com>
2022-07-11 19:23:17 -0700

    Configured view controller for SwiftUI Comment page.

---
+++ ecommerce/ecommerce/CommentPage/Views/CommentSVC.swift
@@ -0,0 +1,20 @@
+//
+//  CommentSVC.swift
+//  ecommerce
+//
+//  Created by Vincent Salinas on 7/11/22.
+//
+
+import SwiftUI
+
+struct CommentSVC: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CommentSVC_Previews: PreviewProvider {
+    static var previews: some View {
+        CommentSVC()
+    }
+}
