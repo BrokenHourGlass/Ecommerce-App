c7ee0c84c2c39a67166fc5fc01449b3719cc2da9
Vincent Salinas <vincent.salinas.2022@gmail.com>
2022-07-12 11:02:42 -0700

    Implemented date component for comments.

---
+++ ecommerce/ecommerce/Utils/Utils.swift
@@ -0,0 +1,18 @@
+//
+//  Utils.swift
+//  ecommerce
+//
+//  Created by Vincent Salinas on 7/12/22.
+//
+
+import Foundation
+
+let dateFormatter = DateFormatter()
+
+
+func formateDate(date: Date) -> String {
+    dateFormatter.dateStyle = .medium
+    dateFormatter.timeStyle = .none
+    dateFormatter.locale = Locale(identifier: "en_US")
+    return dateFormatter.string(from: date)
+}
