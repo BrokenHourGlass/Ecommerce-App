2447344a0afb693e8e8d50ddc6a42edbd9e01f83
Vincent Salinas <vincent.salinas.2022@gmail.com>
2022-07-14 09:18:50 -0700

    Implemented Wishlist Manager.

---
+++ ecommerce/ecommerce/Models/WishlistManager.swift
@@ -0,0 +1,27 @@
+//
+//  WishlistManager.swift
+//  ecommerce
+//
+//  Created by Vincent Salinas on 7/14/22.
+//
+
+import Foundation
+
+class WishlistManager: ObservableObject {
+    @Published private(set) var wishlist: [WishlistItemModel] = []
+    
+    func addToWishlist(wishlistItemObj: WishlistItemModel) {
+        wishlist.append(wishlistItemObj)
+    }
+    
+    func remoteFromWishlist(id: String) {
+        wishlist = wishlist.filter {
+            $0.id != id
+        }
+    }
+    
+    func getWishlist() -> [WishlistItemModel] {
+        return wishlist
+    }
+    
+}
