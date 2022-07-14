7c4235450df795309346e676d75105ef8d7bfd83
Vincent Salinas <vincent.salinas.2022@gmail.com>
2022-07-14 09:06:24 -0700

    Implemented core data functionality to add or remote items from wishlist.

--- ecommerce/ecommerce/CoreData/CDWishlistHelper.swift
+++ ecommerce/ecommerce/CoreData/CDWishlistHelper.swift
@@ -15,20 +15,14 @@ class CDWishlistHelper {
     
     let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext
     
-    func addToWishlist(productObj: NewProduct) {
+    func addToWishlist(wishlistItemObj: WishlistItemModel) {
         
-        let productEntity = NSEntityDescription.insertNewObject(forEntityName: "Product", into: context!) as! Product
+        let wishlistEntity = NSEntityDescription.insertNewObject(forEntityName: "PNPWishlistItem", into: context!) as! PNPWishlistItem
         
-        productEntity.id = Int64(productObj.id)
-        productEntity.name = productObj.name
-        productEntity.category = productObj.category
-        productEntity.new = productObj.new
-        productEntity.price = Int64(productObj.price)
-        productEntity.featured = productObj.featured
-        productEntity.cartIMG = productObj.cartIMG
-        productEntity.productIMG = productObj.productIMG
-        productEntity.pDescription = productObj.description
-        productEntity.features = productObj.features
+        wishlistEntity.id = wishlistItemObj.id
+        wishlistEntity.userId = wishlistItemObj.userId
+        wishlistEntity.productId = Int64(wishlistItemObj.productId)
+        wishlistEntity.status = wishlistItemObj.status
         
         do {
             try context?.save()
@@ -39,120 +33,19 @@ class CDWishlistHelper {
         }
     }
     
-    func addItem(pid: Int, itemObj: NewItem) {
+    func removeFromWishlist(wishlistItemID: String) {
         
-        let itemEntity = NSEntityDescription.insertNewObject(forEntityName: "Item", into: context!) as! Item
-        
-        itemEntity.pid = Int64(pid)
-        itemEntity.item = itemObj.item
-        itemEntity.quantity = Int64(itemObj.quantity)
-        
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "PNPWishlistItem")
+        fetchRequest.predicate = NSPredicate(format: "id == %@", wishlistItemID)
         do {
+            let wishlistItem = try context?.fetch(fetchRequest)
+            context?.delete(wishlistItem?.first as! PNPWishlistItem)
             try context?.save()
-            print("data saved successfully")
+            print("Removed item from wishlist successfully")
         } catch {
-            print("data save unsuccessful")
-            print(error.localizedDescription)
-        }
-    }
-    
-    func addPreview(pid: Int, previewObj: NewPreview) {
-        let previewEntity = NSEntityDescription.insertNewObject(forEntityName: "Preview", into: context!) as! Preview
-        
-        previewEntity.pid = Int64(pid)
-        previewEntity.img = previewObj.img
-        
-        do {
-            try context?.save()
-            print("data saved successfully")
-        } catch {
-            print("data save unsuccessful")
-            print(error.localizedDescription)
-        }
-    }
-    
-    func addRecommended(pid: Int, recommendedObj: NewRecommended) {
-        
-        let recommendedEntity = NSEntityDescription.insertNewObject(forEntityName: "Recommended", into: context!) as! Recommended
-        
-        recommendedEntity.pid = Int64(pid)
-        recommendedEntity.name = recommendedObj.name
-        recommendedEntity.img = recommendedObj.img
-        
-        do {
-            try context?.save()
-            print("data saved successfully")
-        } catch {
-            print("data save unsuccessful")
-            print(error.localizedDescription)
-        }
-    }
-    
-    func displaySearchHistory() {
-        var productData = [Product]()
-        
-        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Product")
-        
-        do {
-            productData = try context?.fetch(fetchRequest) as! [Product]
-        } catch {
-            print("can not fetch data")
+            print("Error detected")
         }
         
-        print(String(productData.count))
     }
-    
-    func getSearchHistory() -> [NewProduct] {
-        var history = [NewProduct]()
-        var productData = [Product]()
-        var itemData = [Item]()
-        var previewData = [Preview]()
-        var recommendedData = [Recommended]()
-        
-        let fetchProducts = NSFetchRequest<NSFetchRequestResult>(entityName: "Product")
-        let fetchItems = NSFetchRequest<NSFetchRequestResult>(entityName: "Item")
-        let fetchPreviews = NSFetchRequest<NSFetchRequestResult>(entityName: "Preview")
-        let fetchRecommended = NSFetchRequest<NSFetchRequestResult>(entityName: "Recommended")
-        
-        do {
-            productData = try context?.fetch(fetchProducts) as! [Product]
-            itemData = try context?.fetch(fetchItems) as! [Item]
-            previewData = try context?.fetch(fetchPreviews) as! [Preview]
-            recommendedData = try context?.fetch(fetchRecommended) as! [Recommended]
-            
-            for x in productData {
-                
-                var itemDataResult = [NewItem]()
-                var previewDataResult = [NewPreview]()
-                var recommendedDataResult = [NewRecommended]()
-                
-                for ite in itemData {
-                    itemDataResult.append(NewItem(quantity: Int(ite.quantity), item: ite.item!))
-                }
-                
-                for pre in previewData {
-                    previewDataResult.append(NewPreview(img: pre.img!))
-                }
-                
-                for rec in recommendedData {
-                    recommendedDataResult.append(NewRecommended(name: rec.name!, img: rec.img!))
-                }
-                
-                history.append(
-                    NewProduct(id: Int(x.id), name: x.name!, category: x.category!, new: x.new, price: Int(x.price), featured: x.featured, cartIMG: x.cartIMG!, productIMG: x.productIMG!, description: x.pDescription!, features: x.features!, contents: itemDataResult, previews: previewDataResult, recommended: recommendedDataResult)
-                )
-            }
-            
-        } catch {
-            print("can not fetch data")
-        }
-        
-        return history
-        
-    }
-    
-    
-    
-    
     
 }
