ba9065cb5fc7e71a8b7acdeb42e286680ecef316
Vincent Salinas <vincent.salinas.2022@gmail.com>
2022-07-16 15:06:45 -0700

    Set up view controller in Storyboard for Account page.

---
+++ ecommerce/ecommerce/AccountPage/Views/AccountPageSVC.swift
@@ -0,0 +1,20 @@
+//
+//  AccountPageSVC.swift
+//  ecommerce
+//
+//  Created by Vincent Salinas on 7/16/22.
+//
+
+import SwiftUI
+
+struct AccountPageSVC: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AccountPageSVC_Previews: PreviewProvider {
+    static var previews: some View {
+        AccountPageSVC()
+    }
+}
