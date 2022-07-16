8bc6fe9b600008953d4e55c35cf9971216607556
Vincent Salinas <vincent.salinas.2022@gmail.com>
2022-07-15 18:23:34 -0700

    Configured view controller in Storyboard for Signup page.

---
+++ ecommerce/ecommerce/SignupPage2/Views/SignupPage2SVC.swift
@@ -0,0 +1,20 @@
+//
+//  SignupPage2SVC.swift
+//  ecommerce
+//
+//  Created by Vincent Salinas on 7/15/22.
+//
+
+import SwiftUI
+
+struct SignupPage2SVC: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SignupPage2SVC_Previews: PreviewProvider {
+    static var previews: some View {
+        SignupPage2SVC()
+    }
+}
---
+++ ecommerce/ecommerce/SignupPage2/Views/SignupPage2VC.swift
@@ -0,0 +1,36 @@
+//
+//  SignupPage2VC.swift
+//  ecommerce
+//
+//  Created by Vincent Salinas on 7/15/22.
+//
+
+import UIKit
+import SwiftUI
+
+class SignupPage2VC: UIViewController {
+    @IBOutlet weak var container: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        let childView = UIHostingController(rootView: SignupPage2SVC())
+        addChild(childView)
+        childView.view.frame = container.bounds
+        container.addSubview(childView.view)
+
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
