f5a3088ebd79b7a750a1f3dadaea36f5e5940c85
Vincent Salinas <vincent.salinas.2022@gmail.com>
2022-07-18 13:49:06 -0700

    Implemented Services object. Includes getProductData function to make api request to fetch data for all products.

---
+++ ecommerce/ecommerce/Services/Services.swift
@@ -0,0 +1,47 @@
+//
+//  Services.swift
+//  ecommerce
+//
+//  Created by Vincent Salinas on 7/18/22.
+//
+
+import Foundation
+
+class Services: ObservableObject {
+    @Published private(set) var productData = [NewProduct]()
+    
+    func getProductData() {
+        let url = "https://raw.githubusercontent.com/BrokenHourGlass/Ecommerce-App/new-main/ecommerce/ecommerce/Resources/new-data.json"
+        let session = URLSession.shared
+        let decoder = JSONDecoder()
+        self.productData = []
+        guard let jsonURL = URL(string: url) else {
+            fatalError("Invalid URL")
+        }
+        let task = session.dataTask(with: jsonURL) { data, response, error in
+            if let error = error {
+                fatalError(error.localizedDescription)
+            }
+            
+            guard let responseData = try? Data(contentsOf: jsonURL) else {
+                print("Error caught: Could not retrieve json data with url provided")
+                return
+            }
+            
+            guard let jsonData = try? decoder.decode([NewProduct].self, from: responseData) else {
+                print("Error caught: Could not decode json data")
+                return
+            }
+            
+            for x in jsonData {
+                print(x.name)
+            }
+            
+            DispatchQueue.main.async {
+                self.productData = jsonData
+            }
+        }
+        
+        task.resume()
+    }
+}
